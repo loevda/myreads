ef300c71d17dfb5f3cb4d7d6210225cdb2418eb0
David Loevenbruck <loevenbruck.david@gmail.com>
2017-07-13 00:59:45 +0200

    refactor: Refactored Books component to use props

--- src/Book.js
+++ src/Book.js
@@ -14,10 +14,13 @@ class Book extends React.Component {
     }
 
     render() {
+
+        const { title, authors, shelf, imageLinks } = this.props
+
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail}` }}></div>
                     <div className="book-shelf-changer">
                         <select>
                             <option value="none" disabled>Move to...</option>
@@ -28,8 +31,12 @@ class Book extends React.Component {
                         </select>
                     </div>
                 </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">
+                    {authors.map((author, index) => (
+                        `${author}<br />`
+                    ))}
+                </div>
             </div>
         )
     }
