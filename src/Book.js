fd1f4cc490aced8549b94dbb65c839508c9091f7
David Loevenbruck <loevenbruck.david@gmail.com>
2017-07-14 14:20:24 +0200

    refactor: Rfeactored Book component select options to have the current book shelf  option selected

--- src/Book.js
+++ src/Book.js
@@ -21,9 +21,9 @@ class Book extends React.Component {
                     <div className="book-shelf-changer">
                         <select>
                             <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
+                            <option value="currentlyReading" selected={book.shelf === 'currentlyReading' ? 'selected' : ''}>Currently Reading</option>
+                            <option value="wantToRead" selected={book.shelf === 'wantToRead' ? 'selected' : ''}>Want to Read</option>
+                            <option value="read" selected={book.shelf === 'read' ? 'selected' : ''}>Read</option>
                             <option value="none">None</option>
                         </select>
                     </div>
