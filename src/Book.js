157d127d9fa75da4ba4a85819e3b630693985426
David Loevenbruck <loevenbruck.david@gmail.com>
2017-07-25 22:49:46 +0200

    refactor: refactored the select input in Book component to use defaultValue instead of selected in option

--- src/Book.js
+++ src/Book.js
@@ -19,11 +19,11 @@ class Book extends React.Component {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail}` }}></div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select defaultValue={book.shelf}>
                             <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading" selected={book.shelf === 'currentlyReading' ? 'selected' : ''}>Currently Reading</option>
-                            <option value="wantToRead" selected={book.shelf === 'wantToRead' ? 'selected' : ''}>Want to Read</option>
-                            <option value="read" selected={book.shelf === 'read' ? 'selected' : ''}>Read</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
                             <option value="none">None</option>
                         </select>
                     </div>
